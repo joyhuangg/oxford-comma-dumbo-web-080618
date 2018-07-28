78ae9177490a8b20facf77db84944425eb759840
joyhuangg <joyhuangg@gmail.com>
2018-07-28 02:12:06 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_element = array.pop()
+    string = array.join(", ")
+    string += ", and #{last_element}"
+    return string
+  end
 end
